67bb9a04f34eede91691acce56d4ce2e2f4b396f
Grashad <awtomat74m@gmail.com>
2025-05-20 00:07:13 +0200

    Working Calculator loop All Clear and Delete done

--- CalcScr.js
+++ CalcScr.js
@@ -9,10 +9,10 @@
     const inputType2 = /(.*[\+\-\/\*=])(?!.[\d])/
     const filtNonDig = /(\D)/g;
     const filtDig = /(\d)/g;
-    const functionB = /(DEL)|(AC)/g;
-
+    const delDEL = /(DEL)/g;
+    const delAC = /(AC)/g;
     /*Function global scope storage*/
-    const calcArray = {
+    let calcArray = {
         value: [],
         value2: [],
         result: [],
@@ -39,91 +39,89 @@
         resultStorage: [],
     }
     let condCheck = 0
+
+
     /* Calculator Functions */
     function calc(input) {
-
         if (condCheck === 0) {
             if (input.match(filtDig) != null) { total = sSS += input }
-            if (input.match(filtDig) === null && total > 0 && condCheck === 0) {
+            if (input.match(delDEL) != null && total > 0) {
+                total = total.slice(0, -1)
+
+            }
+            if (input.match(inputType) != null && total > 0 && condCheck === 0) {
                 arraY.splice(0, 1, total);
                 sSS = "";
                 total = 0;
-                condCheck = 1            
+                condCheck = 1
             }
         }
     }
 
     function storeOp(input) {
-
-        if (input.match(inputType) != null && sOOA.length <= 1 && condCheck === 1 | condCheck === 2) {
+        if (input.match(inputType) != null && sOOA.length <= 1 && condCheck === 1) {
             sLS += input;
             sOOA.splice(0, 1, sLS);
             sLS = "";
-            condCheck = 2  
+            condCheck = 2
         }
     }
 
 
     function calc2(input) {
-        let resArray = historyStorage.resultStorage
         if (input.match(inputType2) === null && condCheck === 2) {
             if (input.match(filtDig) != null) { total2 = sSS += input }
+            if (input.match(delDEL) != null && total2 > 0) {
+                total2 = total2.slice(0, -1)
+            }
         } else if (input.match(inputType2) != null && total2 > 0 && condCheck === 2) {
-            arraY2.splice(0, 1,total2);
+            let resArray = historyStorage.resultStorage
+            arraY2.splice(0, 1, total2);
             sSS = "";
             total = 0;
             ResCalc = arraY[0].concat(sOOA[0], arraY2[0]);
             let resF = Function("return " + ResCalc)();
             resArray.push(resF)
             console.log(`result ${resF}`);
-            condCheck = 0    
+            total = 0;
+            sSS = 0;
+            arraY = [];
+            sLS = "";
+            sOOA = [];
+            total2 = 0;
+            arraY2 = [];
+            condCheck = 0
         }
-        
+
     }
 
-         
-     function display(check) {
-     switch(check) {
-        case 0: {
-        
+    function allClear(input) {
+        if (input.match(delAC) != null) {
+            total = 0;
+            sSS = 0;
+            arraY = [];
+            sLS = "";
+            sOOA = [];
+            total2 = 0;
+            arraY2 = [];
+            resF = [];
+            condCheck = 0;
         }
-        case 1:
-        {
-         
-        }
-        case 2:
-            {
-
-            }
-            case 3:
-                {
-
-                }
-                case 4: {
-
-                }
-                default: {
-
-                }
-     }
-   
-    
     }
-   
 
-   
-    
-  
-   function histStorageAndDelete(input) {
-     
-   }
+
+
+
+
+
+
     for (button of inputButton) {
         button.addEventListener("click", (f) => {
             input = f.target.textContent;
             calc(input);
             storeOp(input);
             calc2(input);
-            display(condCheck)
+            allClear(input)
             console.log(total);
             console.log(total2)
             console.log(sSS)
